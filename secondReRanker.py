c9eaf2fc0e60b731b57121fd9898807d993c8852
denizozkiric <ozkiric.deniz@metu.edu.tr>
2025-01-16 21:45:57 +0300

    complete, untested

--- secondReRanker.py
+++ secondReRanker.py
@@ -4,9 +4,9 @@ from sklearn.neural_network import LinearRegression
 class LinearRegressor:
     def __init__(self):
         self.model = LinearRegression()
-    
+
     def fit(self, X, y):
         self.model.fit(X, y)
-    
+
     def predict(self, X):
         return self.model.predict(X)
--- tests.py
+++ tests.py
@@ -27,19 +27,19 @@ def normaliseRankScores(rel):
 
 
 # MonoT5 results
-
 monoT5_res_tut = pt.io.read_results('monoT5/runs/monot5.titleurltext.res.gz')
 monoT5_res_tu = pt.io.read_results('monoT5/runs/monot5.titleurl.res.gz')
 
 monoT5_res_tu['score'] = normaliseMonoScores(monoT5_res_tu)
 pt.io.write_results(monoT5_res_tu, 'norm_runs/monot5.titleurl.normalized.res.gz')
+monoT5_res_tu_df = pd.DataFrame(monoT5_res_tu)
 
-monoT5_res_tut['score'] = normaliseMonoScores(monoT5_res_tut)
+"""monoT5_res_tut['score'] = normaliseMonoScores(monoT5_res_tut)
 pt.io.write_results(monoT5_res_tut, 'norm_runs/monot5.titleurltext.normalized.res.gz')
+monoT5_res_tut_df = pd.DataFrame(monoT5_res_tut)"""
 
 
 # LambdaMART results
-
 run, qrels, measures_df = lambdaMart()
 
 run_df = pd.DataFrame([{'qid': sd.query_id, 'docid': sd.doc_id, 'score': sd.score} for sd in run])
@@ -51,31 +51,83 @@ pt.io.write_results(qrels_df, 'norm_runs/lambdamart.normalized.qrels.gz')
 
 
 # NeuralRanker results
+grouped_run = run_df.groupby('qid')
+grouped_mono = monoT5_res_tu_df.groupby('qid')
+grouped_qrels = qrels_df.groupby('qid')
 
-mono_res_tu = np.array(monoT5_res_tu['score'])
-#mono_res_tut = np.array(monoT5_res_tut['score'])
-lamb_res = np.array(run_df['score'])
-y = np.array(normaliseRankScores(qrels_df['rel']))
+unique_qids_lambda = run_df['qid'].unique()
+unique_qids_mono = monoT5_res_tu_df['qid'].unique()
 
-X = np.vstack((mono_res_tu, lamb_res)).T  # (n_samples, 2)
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
+split_idx_lambda = len(unique_qids_lambda) // 2
+train_qids_lambda = unique_qids_lambda[:split_idx_lambda]
+test_qids_lambda = unique_qids_lambda[split_idx_lambda:]
+
+split_idx_mono = len(unique_qids_mono) // 2
+train_qids_mono = unique_qids_mono[:split_idx_mono]
+test_qids_mono = unique_qids_mono[split_idx_mono:]
+
+train_run_df = run_df[run_df['qid'].isin(train_qids_lambda)]
+test_run_df = run_df[run_df['qid'].isin(test_qids_lambda)]
+
+train_qrels_df = qrels_df[qrels_df['qid'].isin(train_qids_lambda)]
+test_qrels_df = qrels_df[qrels_df['qid'].isin(test_qids_lambda)]
+
+train_mono_df = monoT5_res_tu_df[monoT5_res_tu_df['qid'].isin(train_qids_mono)]
+test_mono_df = monoT5_res_tu_df[monoT5_res_tu_df['qid'].isin(test_qids_mono)]
+
+# training
+#y = np.array(normaliseRankScores(qrels_df['rel']))
+y_train = np.array(normaliseRankScores(train_qrels_df['rel']))
+# when we add another reranker, must add its rank to here somehow. Average maybe?
+
+X_train = np.vstack((train_mono_df['score'], train_run_df['score'])).T  # (n_samples, 2)
 
 LR = LinearRegressor()
-LR.fit(X, y)
+LR.fit(X_train, y_train)
 
 w1, w2 = LR.model.coef_
 print(f"Learned Weights: w1={w1}, w2={w2}")
 
-# Predict using learned weights
-y_pred = LR.predict(X)
+# testing
+#X_test = np.vstack((test_mono_df['score'], test_run_df['score'])).T  # (n_samples, 2)
+combined_test_df = test_run_df.merge(
+    test_mono_df[['qid', 'docid', 'score']], 
+    on=['qid', 'docid'], 
+    how='left', 
+    suffixes=('_run', '_mono')
+)
+combined_test_df['score_mono'] = combined_test_df['score_mono'].fillna(0)
+X_test = np.vstack((combined_test_df['score_mono'], combined_test_df['score_run'])).T  # (n_samples, 2)
 
-final_scores = w1 * X[:, 0] + w2 * X[:, 1]
-ranked_indices = np.argsort(final_scores)[::-1]  # Sort in descending order
+y_pred = LR.predict(X_test)
 
-run_df['score'] = final_scores
-pt.io.write_results(run_df, 'norm_runs/neuralranker.normalized.res.gz')
+# Evaluate
+# Rank the documents within each query
+combined_test_df['score'] = y_pred
+combined_test_df['rank'] = combined_test_df.groupby('qid')['score'].rank(method='first', ascending=False).astype(int)
+ranked_results = combined_test_df.sort_values(by=['qid', 'rank'])
 
-qrels_df = qrels_df.merge(run_df[['qid', 'docid', 'score']], on=['qid', 'docid'], how='left')
-qrels_df = qrels_df.fillna(0)
-qrels_df['ranked'] = qrels_df.groupby('qid')['score'].rank(method='first', ascending=False).astype(int)
-pt.io.write_results(qrels_df, 'norm_runs/neuralranker.normalized.qrels.gz')
+"""ranked_results = test_run_df.copy()
+ranked_results['score'] = y_pred
+ranked_results['rank'] = ranked_results.groupby('qid')['score'].rank(method='first', ascending=False).astype(int)
+"""
+
+# Write the ranked results to a file
+pt.io.write_results(ranked_results, 'norm_runs/neuralranker.ranked.res.gz')
+
+# Evaluate the ranked results
+eval_metrics = pt.pipelines.Experiment(
+    [
+        pt.pipelines.Evaluate(
+            metrics=[
+                P,
+                nDCG,
+                RR,
+                AP
+            ],
+            perquery=True
+        )
+    ]
+)
+
+eval_metrics(ranked_results, test_qrels_df)
