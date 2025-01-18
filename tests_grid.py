f2c12654211e38dda1067ffb3b3bc0782a41119b
denizozkiric <ozkiric.deniz@metu.edu.tr>
2025-01-18 19:24:25 +0300

    grid search corrected

--- tests_grid.py
+++ tests_grid.py
@@ -11,80 +11,75 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
-grid = {'w_1': [0.1, 0.2, 0.3, 0.4, 0.5]}
-
 
 def seeFormat(res=pt.io.read_results('monoT5/runs/initial.res.gz')):
     print(res.head(10)[['qid', 'docno', 'score', 'rank']])
     # print(res.columns)
 
 
-def normaliseMonoScores(res):
-    normalized_scores = (res['score'] - res['score'].min()) / (res['score'].max() - res['score'].min())
-    return normalized_scores
-
-
-def normaliseRankScores(rel):
-    return 1-(rel - rel.min()) / (rel.max() - rel.min())
-
-
 # MonoT5 results
-monoT5_res_tut = pt.io.read_results('monoT5/runs/monot5.titleurltext.res.gz')
+# monoT5_res_tut = pt.io.read_results('monoT5/runs/monot5.titleurltext.res.gz')
 monoT5_res_tu = pt.io.read_results('monoT5/runs/monot5.titleurl.res.gz')
 
-monoT5_res_tu['score'] = normaliseMonoScores(monoT5_res_tu)
-pt.io.write_results(monoT5_res_tu, 'norm_runs/monot5.titleurl.normalized.res.gz')
+#monoT5_res_tu['score'] = normaliseMonoScores(monoT5_res_tu)
 monoT5_res_tu_df = pd.DataFrame(monoT5_res_tu)
 
-#res = pt.io.read_results('monoT5/runs/initial.res.gz')
-#print(len(monoT5_res_tu_df['qid'].unique()))
+"""monoT5_res_tut['score'] = normaliseMonoScores(monoT5_res_tut)
+pt.io.write_results(monoT5_res_tut, 'norm_runs/monot5.titleurltext.normalized.res.gz')
+monoT5_res_tut_df = pd.DataFrame(monoT5_res_tut)"""
+
 
 # LambdaMART results
-run, qrels, measures_df = lambdaMart()
-
-run_df = pd.DataFrame([{'qid': sd.query_id, 'docid': sd.doc_id, 'score': sd.score} for sd in run])
-run_df['score'] = normaliseMonoScores(run_df)
-pt.io.write_results(run_df, 'norm_runs/lambdamart.normalized.res.gz')
-
-qrels_df = pd.DataFrame([{'qid': q.query_id, 'docid': q.doc_id, 'rel': q.relevance} for q in qrels])
-qrels_df['rel'] = normaliseRankScores(qrels_df['rel'])
-pt.io.write_results(qrels_df, 'norm_runs/lambdamart.normalized.qrels.gz')
+# run, qrels, measures_df = lambdaMart()
+predictions, y_test, q_test = lambdaMart()
 
 
-# GridSearch for weights
-combined_df = run_df.merge(
-    monoT5_res_tu_df[['qid', 'docid', 'score']], 
-    on=['qid', 'docid'], 
-    how='left', 
-    suffixes=('_run', '_mono')
-).assign(score_mono=lambda x: x['score_mono'].fillna(0))
-#combined_df['score_mono'] = combined_df['score_mono'].fillna(0)
+# NeuralRanker results
+unq_qids = np.unique(q_test)
+combined_predictions = [[] for _ in range(len(unq_qids))]
+true_scores = [[] for _ in range(len(unq_qids))]
+X_final = [[] for _ in range(len(unq_qids))]
+index = 0
+for i in range(len(q_test)):
+    combined_predictions[index] = np.append(combined_predictions[index], [[monoT5_res_tu_df["score"][i], predictions[i]]])
+    true_scores[index] = np.append(true_scores[index], y_test[i])
+    if i < len(q_test)-1 and q_test[i+1] != q_test[i]:
+        index += 1
 
-combined_df = combined_df.merge(
-    qrels_df[['qid', 'docid', 'rel']], 
-    on=['qid', 'docid'], 
-    how='left'
-).assign(rel=lambda x: x['rel'].fillna(0))
-#combined_df['rel'] = combined_df['rel'].fillna(0)
+split_indices = len(unq_qids) // 2
 
+combined_predictions_train = combined_predictions[:split_indices]
+combined_predictions_test = combined_predictions[split_indices:]
+true_scores_train = true_scores[:split_indices]
+true_scores_test = true_scores[split_indices:]
 
-X = combined_df[['score_run', 'score_mono']].copy()
-y = combined_df['rel']
+border_qid = unq_qids[split_indices]
+first_test_index = q_test.tolist().index(border_qid)
+
+grid = {'w_1': [0.1, 0.2, 0.3, 0.4, 0.5]}
+
+y_train = np.array(true_scores_train).flatten()
+# when we add another reranker, must add its rank to here somehow. Average maybe?
+
+X_train = np.array(combined_predictions_train).reshape(-1, 2)
+
+X_test = np.array(combined_predictions_test).reshape(-1, 2)
+
+y_test = np.array(true_scores_test).flatten()
 
 min_mse = np.inf
 best_w_1 = None
 for w_1 in grid['w_1']:
-    X['score'] = w_1 * X['score_run'] + (1 - w_1) * X['score_mono']
-    mse = mean_squared_error(y, X['score'])
+    y_pred = w_1 * X_train[:, 0] + (1 - w_1) * X_train[:, 1]
+    mse = mean_squared_error(y_train, y_pred)
     if mse < min_mse:
         min_mse = mse
         best_w_1 = w_1
 
-print(f'Best w_1: {best_w_1}, MSE: {min_mse}')
+print(f"Best w_1: {best_w_1}")
 
-combined_df['score'] = best_w_1 * combined_df['score_run'] + (1 - best_w_1) * combined_df['score_mono']
-#rank
-combined_df = combined_df.sort_values(by=['qid', 'score'], ascending=[True, False])
-combined_df['rank'] = combined_df.groupby('qid').cumcount() + 1
+X_test = np.array(combined_predictions_test).reshape(-1, 2)
+y_pred = best_w_1 * X_test[:, 0] + (1 - best_w_1) * X_test[:, 1]
+mse = mean_squared_error(y_test, y_pred)
 
-pt.io.write_results(combined_df[['qid', 'docid', 'score', 'rank', 'rel']], 'norm_runs/combined.normalized.res.gz')
+print(f"Mean Squared Error: {mse}")
